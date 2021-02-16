4743a176daeec96169fe3dbe0bfb0ab061562b34
Beto <lomba.alberto@hotmail.com>
2021-02-15 21:08:50 -0300

    last

---
+++ DesafioMuxi/app/build/generated/source/buildConfig/androidTest/debug/com/example/desafiomuxi/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.desafiomuxi.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.recyclerteste.test";
+  public static final String BUILD_TYPE = "debug";
+}
